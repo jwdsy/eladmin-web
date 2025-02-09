cbad8afe2efb9b2b0f985bddebfa8fe0ff127538
jwdsy <fly20070315@163.com>
2025-02-09 23:38:34 +0800

    shoping

--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -8,26 +8,15 @@
     <!--表格渲染-->
     <el-table ref="table" v-loading="crud.loading" :data="crud.data" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
       <el-table-column type="selection" width="55" />
-      <el-table-column prop="name" :label="this.$t('product.table.itermNo')" />
-      <el-table-column prop="icon" :label="this.$t('product.table.picture')" align="center" width="250px">
+      <el-table-column prop="itemNo" :label="this.$t('product.table.itermNo')" />
+      <el-table-column prop="itemPic" :label="this.$t('product.table.picture')" align="center" width="250px">
         <template slot-scope="scope">
-          <svg-icon :icon-class="scope.row.icon ? scope.row.icon : ''" />
+          <img :src="scope.row.itemPic ? scope.row.itemPic : ''" style="height: 50px">
         </template>
       </el-table-column>
-      <el-table-column prop="status" label="状态" align="center">
-        <template slot-scope="scope">
-          <el-switch
-            v-model="scope.row.enabled"
-            active-color="#409EFF"
-            inactive-color="#F56C6C"
-            @change="changeEnabled(scope.row, scope.row.enabled)"
-          />
-        </template>
-      </el-table-column>
-      <el-table-column prop="createTime" label="创建日期" />
       <!--   编辑与删除   -->
       <el-table-column
-        v-if="checkPer(['admin','job:edit','job:del'])"
+        v-if="checkPer(['admin','product:edit','product:del'])"
         label="操作"
         width="130px"
         align="center"
@@ -49,7 +38,7 @@
 </template>
 
 <script>
-import crudJob from '@/api/system/job'
+import crudJob from '@/api/product'
 import eHeader from './module/header'
 import eForm from './module/form'
 import CRUD, { presenter } from '@crud/crud'
@@ -62,7 +51,7 @@ export default {
   cruds() {
     return CRUD({
       title: '产品',
-      url: 'item/detail/v1/getItemDetailList',
+      url: 'api/product',
       sort: ['jobSort,asc', 'id,desc'],
       crudMethod: { ...crudJob }
     })
@@ -73,9 +62,9 @@ export default {
   data() {
     return {
       permission: {
-        add: ['admin', 'job:add'],
-        edit: ['admin', 'job:edit'],
-        del: ['admin', 'job:del']
+        add: ['admin', 'product:add'],
+        edit: ['admin', 'product:edit'],
+        del: ['admin', 'product:del']
       }
     }
   },
